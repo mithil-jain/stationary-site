71a0fbfda8fd765741a2c71a51da088d36b78a04
mithil-jain <mithiljain8@gmail.com>
2018-09-20 13:22:46 +0530

    index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <title>Login</title>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<link rel="stylesheet" type="text/css" href="Addons/index.css">
 	<script>
 		function forgot(){
